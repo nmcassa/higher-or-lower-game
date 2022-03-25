c76d23fd6d7a08f11c6d5d8b965940c3f85c0293
nmcassa <nmcassa804@outlook.com>
2022-03-25 15:35:34 -0400

    v.04
    higher or lower buttons
    changing teams every button click

--- static/index.js
+++ static/index.js
@@ -1,14 +1,39 @@
 eLeft = document.getElementById('left');
 eRight = document.getElementById('right');
+eLeftW = document.getElementById('left-winnings');
 listOfRLTeams = [];
+teamIndex = 0;
 
 fetch('/test')
       .then(function (response) {
-          return response.json();
+        if (!response.ok) {
+          throw new Error("HTTP error, status = " + response.status);
+        }
+        return response.json();
       }).then(function (text) {
           console.log('GET response:');
           console.log(text.message);
-          listOfRLTeams = text.message;
-          eLeft.innerHTML = listOfRLTeams[0];
-          eRight.innerHTML = text.message[1];
+          myDisplayer(text.message);
       });
+
+async function myDisplayer(some) {
+  listOfRLTeams = await some;
+
+  //splits the list into a 2D array with 
+  //each row containing [name][winnings]
+  for (let i = 0; i < listOfRLTeams.length; i++) {
+    listOfRLTeams[i] = listOfRLTeams[i].split(":");
+  }
+
+  //shows first two teams
+  eLeft.innerHTML = listOfRLTeams[teamIndex][0];
+  eLeftW.innerHTML = listOfRLTeams[teamIndex++][1];
+  eRight.innerHTML = listOfRLTeams[teamIndex][0];
+}
+
+function button_clicked(choice) {
+  //choice = 0 if higher, choice = 1 if lower
+  eLeft.innerHTML = eRight.innerHTML;
+  eLeftW.innerHTML = listOfRLTeams[teamIndex++][1];
+  eRight.innerHTML = listOfRLTeams[teamIndex][0];
+}
